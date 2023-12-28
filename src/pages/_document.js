7bf38ca5e484f25962bdd1e73db1282ae6e525d0
Devanshu Kejriwal <devanshukejriwal@Devanshus-MacBook-Air.local>
2023-12-28 19:37:22 +0530

    Fixed url'sname

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -36,11 +36,12 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
           />
           <link rel="icon" type="image/png" href={"/favicon.png"} />
-          <title>Portfolio sample</title>
+          <title>Devanshu's Portfolio</title>
         </Head>
 
         <body>
           <Main />
+          
           <NextScript />
           <div id="modal-root" />
         </body>
